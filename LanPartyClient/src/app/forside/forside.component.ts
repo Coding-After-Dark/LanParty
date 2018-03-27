b15b0f81bd36a4a84a7dbd62294d03a77bfa5537
Bwarhness <nalar94@gmail.com>
2018-03-27 16:43:33 +0200

    desktop code
    your stuff from desktop

--- LanPartyClient/src/app/forside/forside.component.ts
+++ LanPartyClient/src/app/forside/forside.component.ts
@@ -11,7 +11,7 @@ export class ForsideComponent implements OnInit {
   title: any;
   connection;
   test;
-  procent = 'Yo';
+  procent;
 
   constructor(_connectionService: ConnectionService, 
     public _gameService: GameService, 
@@ -19,39 +19,11 @@ export class ForsideComponent implements OnInit {
     private ref: ChangeDetectorRef) {
     this.title = _connectionService.serverIP;
 
-
-
-    if (this._electronService.isElectronApp) {
-      this._electronService.ipcRenderer.on('stopDownloading', function (event, data) {
-        let bla = _gameService.games.find(it => {
-          return it.name.toLowerCase().includes(data.toLowerCase());
-        });
-        bla.isDownloading = false;
-        bla.procent = "installed";
-        ref.detectChanges();
-      });
-
-      this._electronService.ipcRenderer.on('updateP', (event, data) => {
-        let bla = _gameService.games.find(it => {
-          return it.name.toLowerCase().includes(data.name.toLowerCase());
-        });
-        bla.isDownloading = false;
-        bla.procent = data.procent + "%";
-        ref.detectChanges();
-        console.log(bla);
-      });
-    }
   }
 
   ngOnInit() {
   }
-  getGame(Game) {
-    if (this._electronService.isElectronApp) {
-      Game.isDownloading = true;
-      this._electronService.ipcRenderer.send('getGame', Game.name);
-      alert('Du er nu i gang med at downloade et spil (' + Game.name + '), hurra!');
-    }
-  }
+
   uploadGame() {
     if (this._electronService.isElectronApp) {
       this._electronService.ipcRenderer.send('selectGame');
